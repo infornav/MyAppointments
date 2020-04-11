eff33d89ea3c8f69075eed577dd7fe881e7abbcd
Abel <ruffo_roque@hotmail.com>
2020-04-10 20:47:25 -0500

    Fix scheduled date (editText)

--- app/src/main/java/com/sisternav/myappointments/ui/CreateAppointmentActivity.kt
+++ app/src/main/java/com/sisternav/myappointments/ui/CreateAppointmentActivity.kt
@@ -214,11 +214,12 @@ class CreateAppointmentActivity : AppCompatActivity() {
         val listener = DatePickerDialog.OnDateSetListener{ datePicker, y, m, d ->
             //Toast.makeText(this,"$y-$m-$d",Toast.LENGTH_SHORT).show()
             selectedCalendar.set(y,m,d)
+
             etScheduledDate.setText(
                 resources.getString(
                     R.string.date_format,
                     y.toString(),
-                    m.twoDigits(),
+                    (m + 1).twoDigits(),
                     d.twoDigits()
                 )
             )
